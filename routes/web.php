649ac3da2b73bfc746948ef9fcfa2a3d6831c6fc
ForMe1970 <ForMe1970@163.com>
2021-04-12 21:38:57 +0800

    自动部署

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::post('hook', function() {
+    system('sudo cd /var/www/html/blog');
+    system('sudo unset GIT_DIR');
+    system('sudo git pull');
+});
 
 Route::get('/', function () {
     return view('welcome');
